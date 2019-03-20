4b1d65e04c7ad737bc008e0a51949d14847a93e3
chintyadewi <chintyadewi437@gmail.com>
2019-03-20 16:16:31 +0700

    fix navigation dropdown

--- config/styles.php
+++ config/styles.php
@@ -8,5 +8,5 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
 <!-- add file css here! -->
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/login.css">
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/login.css">
