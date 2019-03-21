eb98a888b4da679b5c1428a41caac1d2fa638834
hafizhdiaz <hafizhdiaz@gmail.com>
2019-03-21 23:07:08 +0700

    Home Mahasiswa Fix

--- module/mahasiswa/home.php
+++ module/mahasiswa/home.php
@@ -3,71 +3,110 @@
     <div class="col-md-3 p-0 ">
       <div class="sticky-sidebar sticky-top">
           <div class="m-2 p-3 bg-white rounded shadow-sm">
+          <h5><strong>Pencarian Berita</strong></h5>
           <div id="datepicker"></div>
           </div>
       </div>
     </div>
 
-    <div class="col-md-6 p-0">
+    <div class="col-md-6 p-0 pb-3">
       <div class="m-2 p-3 bg-white rounded shadow-sm">
-          <div class="border-bottom border-gray">
-            <div class="row">
-              <div class="col-sm-10">
-                <h5 >5 Dosen dikirim ke Jepang</h5>
+        <div class="border-bottom border-gray">
+          <div class="row">
+            <div class="col-sm-10">
+              <div class="judul pl-3">
+                <h5><strong>5 Dosen dikirim ke Jepang</strong></h5>
                 <p>6 Maret 2019</p>
               </div>
-              <div class="col-sm-2 mt-2">
-                <span id="kategori">Berita</span>
-              </div>
-            </div>  
-          
-
-
-          </div>
-          
-          <div class="media text-muted pt-3">
+            </div>
+            <div class="col-sm-2 mt-2">
+              <span class="kategori">Berita</span>
+            </div>
+          </div>  
+        </div>
+        <div class="media text-muted pt-3">
           <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              
-              <div class="isi">
+              <div class="isi-mhs pl-3">
               Dikarenakan dosen bernama : <br>
               - Pak Yan <br>
               - Pak Rosa <br>
               - Pak Vipkas <br>
-              sedang ditugaskan ke jepang, maka kelas yang diajar libur. <br><br>
+              sedang di tugaskan ke jepang maka kelas yang diajar oleh dosen-dosen tersebut diharap segera menghubungi untuk meminta tugas. <br><br>
               Terima Kasih...
               </div>
           </div>
-          
-          </div>
-          <div class="form-group">
-            <label for=""></label>
-            <textarea class="form-control border-0" name="" id="" rows="auto" placeholder="Tulis Komentar..."></textarea>
-          </div>
-          <small class="d-block text-right mt-3">
-          <a href="#">All suggestions</a>
-          </small>
+        </div>
+        <div class="form-group border-bottom border-gray">
+          <textarea class="form-control border-0" name="" id="" rows="auto" placeholder="Tulis Komentar..."></textarea>
+        </div>
+        <div class="media-body pb-3 mb-0 small lh-125">
+              <div class="isi-mhs pl-3">
+              <strong>Pak_Yan123</strong>  <span class="komen pl-3">Mantappp minn ...</span> <br>
+              <strong>Veronica_imoet</strong>  <span class="komen pl-3">Lanjutkan min!!!</span> <br>
+              <strong>Sabyan_Lovers</strong>  <span class="komen pl-3">Terima Kasih</span> <br>
+              </div>
+        </div>  
       </div>
+      <div class="m-2 p-3 bg-white rounded shadow-sm">
+          <div class="border-bottom border-gray">
+            <div class="row">
+              <div class="col-sm-10">
+                <div class="judul pl-3">
+                  <h5><strong>5 Dosen dikirim ke Jepang</strong></h5>
+                  <p>6 Maret 2019</p>
+                </div>
+              </div>
+              <div class="col-sm-2 mt-2">
+                <span class="kategori">Berita</span>
+              </div>
+            </div>  
+          </div>
+        <div class="media text-muted pt-3">
+          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <div class="isi-mhs pl-3">
+            Dikarenakan dosen bernama : <br>
+            - Pak Yan <br>
+            - Pak Rosa <br>
+            - Pak Vipkas <br>
+            sedang di tugaskan ke jepang maka kelas yang diajar oleh dosen-dosen tersebut diharap segera menghubungi untuk meminta tugas. <br><br>
+            Terima Kasih...
+            </div>
+          </div>
+        </div>
+        <div class="form-group border-bottom border-gray">
+          <textarea class="form-control border-0" name="" id="" rows="auto" placeholder="Tulis Komentar..."></textarea>
+        </div>
+        <div class="media-body pb-3 mb-0 small lh-125">
+            <div class="isi-mhs pl-3">
+              <strong>Pak_Yan123</strong>  <span class="komen pl-3">Mantappp minn ...</span> <br>
+              <strong>Veronica_imoet</strong>  <span class="komen pl-3">Lanjutkan min!!!</span> <br>
+              <strong>Sabyan_Lovers</strong>  <span class="komen pl-3">Terima Kasih</span> <br>
+            </div>
+        </div>  
       </div>
+    </div>
 
-      <div class="col-md-3 p-0">
+    <div class="col-md-3 p-0">
       <div class="sticky-sidebar sticky-top">
-          <div class="m-2 p-3 bg-white rounded shadow-sm">
-          <h6 class="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
-          <div class="media text-muted pt-3">
-              <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <div class="d-flex justify-content-between align-items-center w-100">
-                  <strong class="text-gray-dark">Full Name</strong>
-                  <a href="#">Follow</a>
-              </div>
-              <span class="d-block">@username</span>
-              </div>
+        <div class="m-2 p-3 bg-white rounded shadow-sm">
+          <div class="beasiswa pb-3 mb-0 border-bottom border-dark">
+            <h5 class="text-center"><strong>Info Beasiswa</strong></h5>
+            <h5><strong>Beasiswa Astra</strong></h5>
+            <p class="isi-beasiswa">PT Astra Internasional, Tbk memiliki 2 beasiswa kuliah S1 yang diberikan untuk kamu. Pertama adalah Beasiswa Astra 1st yang diberikan pada mahasiswa yang berkuliah di Pulau Jawa dan diutamakan dari jurusan Teknik, Ekonomi, Psikologi, IT, dan Statistik yang berada di semester 2,4, atau 6.</p>
+            <small class="d-block text-right mt-3 ">
+                <button class="check btn"><a href="#">Check Link</a></button>
+            </small>
           </div>
-          <small class="d-block text-right mt-3">
-              <a href="#">All suggestions</a>
-          </small>
+          <div class="beasiswa pt-3 pb-3 mb-0 border-bottom border-dark">
+            <h5><strong>Beasiswa Astra</strong></h5>
+            <p class="isi-beasiswa">PT Astra Internasional, Tbk memiliki 2 beasiswa kuliah S1 yang diberikan untuk kamu. Pertama adalah Beasiswa Astra 1st yang diberikan.</p>
+            <small class="d-block text-right mt-3 ">
+                <button class="check btn"><a href="#">Check Link</a></button>
+            </small>
           </div>
+        </div>
       </div>
-      </div>
+    </div>
 
       
 
