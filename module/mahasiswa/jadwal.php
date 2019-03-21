3ee1c182b2e9a35b33926d20024a01c91902bff7
bluohazard <greggygf@gmail.com>
2019-03-21 21:27:58 +0700

    edit jadwal

--- module/mahasiswa/jadwal.php
+++ module/mahasiswa/jadwal.php
@@ -1,1 +1,70 @@
-<h1>Halaman Jadwal</h1>
+<div class="row">
+    <div class="col-md-12">
+        <div class="m-2 p-3 bg-white rounded shadow-sm">
+            <h3>JADWAL PERKULIAHAN</h3>
+        </div>
+    </div>
+</div>
+
+<main role="main" class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 p-0">
+            <div class="m-2 p-3 bg-white rounded shadow-sm">
+                <div class="media text-muted pt-3">
+                    <div class="media-body pb-3 mb-0 small lh-125">
+                        <center><img src="../attachment/img/avatar.jpeg" class="gambar-profil img-circle" height="250"
+                                width="250"></center>
+                        <br>
+                        <h5 class="border-bottom border-gray" align="center">NAMA</h5>
+                        <h5 class="border-bottom border-gray" align="center">NIM MHS</h5>
+                        <h5 class="border-bottom border-gray" align="center">PRODI</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9 p-0">
+            <div class="m-2 p-3 bg-white rounded shadow-sm">
+                <h6 class="border-bottom border-gray pb-2 mb-0">Jadwal Kuliah</h6>
+                <div class="media text-muted pt-3">
+                    <p class="media-body pb-3 mb-0 small lh-125">
+                        <strong class="d-block text-dark">Periode Semester</strong>
+                    </p>
+                </div>
+                <select class="custom-select" style="width:250px">
+                    <option selected>-</option>
+                    <option value="1">Semester 1</option>
+                    <option value="2">Semester 2</option>
+                    <option value="3">Semester 3</option>
+                    <option value="3">Semester 4</option>
+                    <option value="3">Semester 5</option>
+                    <option value="3">Semester 6</option>
+                    <option value="3">Semester 7</option>
+                    <option value="3">Semester 8</option>
+                </select>
+                <button type="button" class="btn btn-success">Filter</button><br><br>
+
+                <table class="table table-striped table-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama Mata Kuliah</th>
+                            <th scope="col">Dosen</th>
+                            <th scope="col">Hari</th>
+                            <th scope="col">SKS/JAM</th>
+                            <th scope="col">Ruang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Matkul A</td>
+                            <td>Dosen A</td>
+                            <td>Senin</td>
+                            <td>2 SKS / 4 JAM</td>
+                            <td>KB 02 (Lt7)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
