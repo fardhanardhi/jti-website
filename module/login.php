bc375f12e0c14c83586f66d05d6ad6dbd52daeca
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2019-03-22 19:32:44 +0700

    include styles.php

--- module/login.php
+++ module/login.php
@@ -8,18 +8,9 @@
 
   <title>Masuk - JTI Website</title>
 
-  <!-- bootstrap -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <!-- fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
-
-  <!-- icons -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-  <!-- add file css here! -->
-  <link rel="stylesheet" href="../css/style.css">
-  <link rel="stylesheet" href="../css/login.css">
+  <?php
+    include '../config/styles.php';
+  ?>
 </head>
 
 <body>
