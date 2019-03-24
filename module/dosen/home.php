8512b4c06d53b7111425161cfe89fe75adb1587a
Navalino Vian <viansatria57@gmail.com>
2019-03-24 19:38:43 +0700

    homepage dosen sementara

---
+++ module/dosen/home.php
@@ -0,0 +1,149 @@
+<main role="main" class="container-fluid">
+    <link rel="stylesheet" href="../css/dosen.css">
+  <div class="row">
+      <!-- Profil Dosen -->
+    <div class="col-md-3 p-0 ">
+      <div class="sticky-sidebar sticky-top">
+          <div class="m-2 p-3 bg-white rounded shadow-sm">
+            <div class="media text-muted pt-3">
+            <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">              
+                <div class="isi">
+                    <div class="d-flex justify-content-center">
+                        <img src="../attachment/img/avatar.jpeg" alt="dosen" style="width:150px;height:150px;border-radius:50%;" >
+                    </div>
+                    <div class="data-dosen text-center">
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">BABANG NICHOL</h6>
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">1984757018014</h6>
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">JABATAN FUNGSIONAL</h6>
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">PENDIDIDIKAN</h6>
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">STATUS IKATAN KERJA</h6>
+                        <h6 class="detail-dosen border-bottom border-gray pb-2 mb-0">STATUS AKTIVITAS</h6>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <small class="d-block text-right mt-3">
+            <a href="#">All suggestions</a>
+            </small>
+          </div>
+      </div>
+    </div>
+    <!-- Jadwal Dosen -->
+    <div class="col-md-6 p-0">
+      <div class="m-2 p-3 bg-white rounded shadow-sm">
+          <h6 class="border-bottom border-gray pb-2 mb-0 judul">JADWAL DOSEN</h6>
+          <div class="media text-muted pt-3">
+          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">              
+              <div class="isi">
+                <table class="table table-striped table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Mata Kuliah</th>
+                            <th>Hari</th>
+                            <th>Waktu</th>
+                            <th>SKS</th>
+                            <th>Ruang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Basis Data</td>
+                            <td>Sabtu</td>
+                            <td>09:00 - 09:01</td>
+                            <td>26</td>
+                            <td>LIP3(Lt.9)</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+          </div>
+          </div>
+          <small class="d-block text-right mt-3">
+          <a href="#">All suggestions</a>
+          </small>
+      </div>
+                    
+      </div>
+    <!-- Kompen Mahasiswa -->
+    <div class="col-md-3 p-0 ">
+      <div class="sticky-sidebar sticky-top">
+          <div class="m-2 p-3 bg-white rounded shadow-sm">
+          <h6 class="border-bottom border-gray pb-2 mb-0 judul">KOMPEN MAHASISWA</h6>    
+                <div class="media text-muted pt-3">
+                <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">              
+                    <div class="isi">
+                        <div class="d-flex justify-content-center">
+                            <img src="../img/clipboard.svg" alt="clipBoard">
+                        </div>
+                        <div class="data-kompen text-center">
+                        
+                        </div>
+                    </div>
+                </div>
+                </div>
+            <small class="d-block text-right mt-3">
+            <a href="#">All suggestions</a>
+            </small>
+          </div>
+      </div>
+    </div>
+  </div>
+</main>
