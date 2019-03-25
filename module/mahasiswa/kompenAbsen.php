ca3539a01d16292c08ec5b3c4b3f3af16809f201
syahdanny <syahdanny11@gmail.com>
2019-03-25 23:17:24 +0700

    commit kompen

--- module/mahasiswa/kompenAbsen.php
+++ module/mahasiswa/kompenAbsen.php
@@ -3,13 +3,57 @@
     <div class="col-md-12 p-0">
       <div class="m-2 bg-white shadow-sm rounded">
         <nav class="nav nav-underline">
-          <span class="nav-link">KARTU HASIL STUDI</span>
+            <div class="col-md-1"></div>
+            <div class="col-md-2 pt-3 pb-3 pr-0">
+            <img src="../attachment/img/avatar.jpeg" class="gambar-profil img-circle" height="150" width="150">
+            
+            </div>
+            <div class="col-md pt-3 pb-3 pl-0">
+              <h1>Avatar</h1>
+              <div class="col-sm">A</div>
+              <div class="col-sm">I</div>
+              <div class="col-sm">S</div>
+            </div>
+            <div class="col-md-6"></div>
         </nav>
-      </div>
-    </div>
+        <nav class="nav nav-underline">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+        <div class="col-md p-0">
+          <button type="button" class="btn btn-success">Filter</button><br><br>
 
-    <div class="circle">
-      <i class="fas fa-comment-alt"></i>
+          <table class="table table-striped">
+            <thead class="bg-primary text-white">
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Tanggal</th>
+                <th scope="col">Jenis Kompensasi</th>
+                <th scope="col">Total Jam</th>
+                <th scope="col">Dosen</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>1 Januari 2019</td>
+                <td>Dosen A</td>
+                <td>2</td>
+                <td>Dosen</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>1 Januari 2019</td>
+                <td>Dosen B</td>
+                <td>3</td>
+                <td>Dosen</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>1 Januari 2019</td>
+                <td>Dosen C</td>
+                <td>8</td>
+                <td>Dosen</td>
+              </tr>
+            </tbody>
+          </table>
     </div>
-  </div>
-</main>
