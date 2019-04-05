4c19a993219c294eee61033f2d1d9f1f3b7fa904
hafizhdiaz <hafizhdiaz@gmail.com>
2019-04-05 07:57:44 +0700

    memperbarui home mhs

--- module/mahasiswa/home.php
+++ module/mahasiswa/home.php
@@ -271,14 +271,17 @@
               <div class="col-sm-1">
 
               </div>
-              <div class="col-sm-8 border-left border-dark">
-                <strong>Admin</strong> <span class="komen">Dosen pergi ke jepang dalam rangka sebagai perwakilan
-                  indonesia di pertemuan KTT 2019 </span> <br>
-                <strong>Sabyan_Lovers</strong> <span class="komen">Okeke min</span> <br>
-                <div class="form-group border border-dark komen2 ">
-                  <textarea class="form-control border-0" name="" id="" rows="1"
-                    placeholder="Tulis Komentar..."></textarea>
+              <div class="col-sm-8 ">
+                <div class="balas-komen p-2 border-left border-dark">
+                  <strong>Admin</strong> <span class="komen">Dosen pergi ke jepang dalam rangka sebagai perwakilan
+                      indonesia di pertemuan KTT 2019</span> <br>
+                  <strong>Sabyan_Lovers</strong> <span class="komen">Okeke min</span> <br>
                 </div>
+              <div class="form-group border border-dark komen2 ">
+                <textarea class="form-control border-0" name="" id="" rows="1"
+                  placeholder="Tulis Komentar..."></textarea>
+              </div>
+                
               </div>
               <div class="col-sm-2">
                 <strong>Reply</strong>
@@ -434,8 +437,17 @@
               - Pak Vipkas <br>
               sedang di tugaskan ke jepang maka kelas yang diajar oleh dosen-dosen tersebut diharap segera menghubungi
               untuk meminta tugas. <br><br>
-              Terima Kasih...
-              <a href="">
+              Terima Kasih... <br>
+                <div class="row isi-download">
+                  <div class="col-md-12">
+                    <button class="btn btn-outline-dark download d-flex justify-content-around">
+                      <h5><a href="" class="pl-0">Dokumen Rahasia</a></h5>
+                      <img src="../img/vector.svg" alt="Download button" class="pl-0">
+                    </button>
+                </div>
+                </div>
+              
+              <!-- <a href="">
                 <div class="download border border-dark mt-2 p-3">
                   <div class="row isi-download">
                     <div class="col-sm-7">
@@ -446,7 +458,7 @@
                     </div>
                   </div>
                 </div>
-              </a>
+              </a> -->
             </div>
           </div>
         </div>
