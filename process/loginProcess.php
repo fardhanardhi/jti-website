3c801cc8482be1fdb07834dd72adc7d2f601c632
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-04-09 10:06:17 +0700

    bersih-bersih file index & proses

--- process/loginProcess.php
+++ process/loginProcess.php
@@ -2,43 +2,40 @@
 session_start();
 include "../config/connection.php";
 
-  if(isset($_POST["dosen"])){
-    $_SESSION["level"]="dosen";
-    $_SESSION["id"]=3;
-    header("location: ../index.php");
-  }
-  else if(isset($_POST["mahasiswa"])){
-    $_SESSION["level"]="mahasiswa";
-    $_SESSION["id"]=2;
-    header("location: ../index.php");
-  }
-  else if(isset($_POST["admin"])){
-    $_SESSION["level"]="admin";
-    $_SESSION["id"]=1;
-    header("location: ../index.php");
-  }
+if (isset($_POST["dosen"])) {
+  $_SESSION["level"] = "dosen";
+  $_SESSION["id"] = 3;
+  header("location: ../index.php");
+} else if (isset($_POST["mahasiswa"])) {
+  $_SESSION["level"] = "mahasiswa";
+  $_SESSION["id"] = 2;
+  header("location: ../index.php");
+} else if (isset($_POST["admin"])) {
+  $_SESSION["level"] = "admin";
+  $_SESSION["id"] = 1;
+  header("location: ../index.php");
+}
 
-  if (isset($_POST["submit"])==true) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+if (isset($_POST["submit"]) == true) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
-    $queryUser = "SELECT * FROM tabel_user WHERE username='$username'";
-    $resultUser = mysqli_query($con, $queryUser);
+  $queryUser = "SELECT * FROM tabel_user WHERE username='$username'";
+  $resultUser = mysqli_query($con, $queryUser);
 
-    if(mysqli_num_rows($resultUser) == 1) {
-      $row = mysqli_fetch_assoc($resultUser);
+  if (mysqli_num_rows($resultUser) == 1) {
+    $row = mysqli_fetch_assoc($resultUser);
 
-      if ($password != $row["password"]) {
-        $error = "*Password salah";
-        header("Location: ../module/login.php?error=$error");
-      } else {
-        $_SESSION["id"]=$row["id_user"];
-        $_SESSION["level"]=$row["level"];
-        header("location: ../index.php");
-      }
-    }else{
-      $error = "*Username tidak ditemukan";
-        header("Location: ../module/login.php?error=$error");
+    if ($password != $row["password"]) {
+      $error = "*Password salah";
+      header("Location: ../module/login.php?error=$error");
+    } else {
+      $_SESSION["id"] = $row["id_user"];
+      $_SESSION["level"] = $row["level"];
+      header("location: ../index.php");
     }
+  } else {
+    $error = "*Username tidak ditemukan";
+    header("Location: ../module/login.php?error=$error");
   }
-?>
+}
