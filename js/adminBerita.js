ee270f84b55da7bdf049bed86f87a69a5ec34e73
Navalino Vian <viansatria57@gmail.com>
2019-04-15 10:48:17 +0700

    memperbaiki char count

--- js/adminBerita.js
+++ js/adminBerita.js
@@ -1,12 +1,7 @@
+Beritacharcountupdate('');
+
 function Beritacharcountupdate(beritacount){
 	var lng = beritacount.length;
 	document.getElementById("Bercharcount").innerHTML = lng + '/500';
 }
 
-function pageRefresh() {
-	Beritacharcountupdate("");
-  }
-
-window.onload = function() {
-	pageRefresh();
-};
