1dd7c1168a37d14a6753b0db23e3ef5159f22eee
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-04-16 11:21:27 +0700

    memperbaiki proses tampil chat e-complain

--- process/proses_eComplain.php
+++ process/proses_eComplain.php
@@ -3,6 +3,20 @@ include "../config/connection.php";
 
 function tampilChat($con, $idUser)
 {
+  $chat =
+    "SELECT
+      *
+    FROM
+      tabel_chat
+    WHERE
+      pengirim = $idUser AND penerima = 2 OR pengirim = 2 AND penerima = $idUser
+    ORDER BY
+      waktu
+    ";
+
+  $resultChat = mysqli_query($con, $chat);
+  return $resultChat;
+}
   $chat = "SELECT * FROM tabel_chat WHERE pengirim = $idUser or penerima = $idUser ORDER BY waktu";
   $resultChat = mysqli_query($con, $chat);
   return $resultChat;
