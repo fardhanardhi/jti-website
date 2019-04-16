640e2edc9394549fc715ddef0e82f615c4849ee3
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-04-16 11:22:57 +0700

    menambah fungsi proses recent chat

--- process/proses_eComplain.php
+++ process/proses_eComplain.php
@@ -17,7 +17,91 @@ function tampilChat($con, $idUser)
   $resultChat = mysqli_query($con, $chat);
   return $resultChat;
 }
-  $chat = "SELECT * FROM tabel_chat WHERE pengirim = $idUser or penerima = $idUser ORDER BY waktu";
+
+function tampilRecentChat($con, $idUser)
+{
+  $chat =
+    "SELECT
+      id_chat,
+      isi,
+      b.recent_user,
+      waktu
+    FROM (
+      SELECT
+        id_chat,
+        isi,
+        pengirim,
+        penerima,
+        IF (
+          pengirim = 32,
+          penerima,
+          pengirim
+        ) 
+        AS 
+        recent_user,
+        waktu
+      FROM
+        tabel_chat
+      WHERE
+        waktu IN (
+        SELECT
+          MAX(waktu)
+        FROM
+          tabel_chat
+        GROUP BY
+          IF (
+          pengirim = 32,
+          penerima,
+          pengirim
+        )
+      ) 
+    ) AS b
+    ORDER BY
+      waktu
+    DESC
+    ";
+
+  /* 
+SELECT
+      id_chat,
+      isi,
+      b.recent_user,
+      waktu
+    FROM (
+      SELECT
+        id_chat,
+        isi,
+        pengirim,
+        penerima,
+        IF (
+          pengirim = 32,
+          penerima,
+          pengirim
+        ) 
+        AS 
+        recent_user,
+        waktu
+      FROM
+        tabel_chat
+      WHERE
+        waktu IN (
+        SELECT
+          MAX(waktu)
+        FROM
+          tabel_chat
+        GROUP BY
+          IF (
+          pengirim = 32,
+          penerima,
+          pengirim
+        )
+      ) 
+    ) AS b
+    ORDER BY
+      waktu
+    DESC
+*/
+
   $resultChat = mysqli_query($con, $chat);
   return $resultChat;
 }
