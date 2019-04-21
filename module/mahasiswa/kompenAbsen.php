17793d29e4d34179a9611152db1443fab913f4b5
Reffan Pandu Amirulloh <reffanpanduamirulloh@Reffans-MacBook-Pro.local>
2019-04-21 21:19:24 +0700

    nambah modal (- ditata)

--- module/mahasiswa/kompenAbsen.php
+++ module/mahasiswa/kompenAbsen.php
@@ -35,7 +35,7 @@
         <div class="container-fluid p-0 m-0">
           <div class="row">
             <div class="col">
-              <button type="button" class="btn btn-primary tmbh-kmpn">Tambah Kompen</button>
+              <button type="button" class="btn btn-primary tmbh-kmpn" data-target="#kompenTambah" data-toggle="modal">Tambah Kompen</button>
               <button type="button" class="btn btn-success float-right tbl-info" data-target="#informasiKompen" data-toggle="modal">Informasi</button>  
               <br>
               <br>
@@ -87,16 +87,14 @@
     </div>
 
 <!-- The modal pengaturan -->
-<div class="modal fade" id="informasiKompen" tabindex="-1" role="dialog" aria-labelledby="modalLabelLarge"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-body pb-0">
-          <h5 class="modal-title text-center">Informasi</h5>
-          <hr class="pl-4 pr-4 bg-dark">
-
-          <div class="row">
-            <div class="col-md-12">
+<div class="modal fade" id="informasiKompen" tabindex="-1" role="dialog" aria-labelledby="modalLabelLarge" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-body">
+        <h5 class="modal-title text-center">Informasi</h5>
+        <hr class="pl-4 pr-4 bg-dark">
+        <div class="row">
+          <div class="col-md-12">
             <h6>1. Kompen = Alpha x 2</h6>
             <br>
             <h6>2. Jika mahasiswa ingin membayar kompen sebagai pengganti pekerjaan maka membayar Rp.10.000/Jam</h6>
@@ -104,9 +102,60 @@
             <h6>3. Kompen akan dikalikan 2(dua) jika kompen pada semester sebelumnya belum diselesaikan</h6>
           </div>
         </div>
-        <div class="align-self-end p-2 ">
-          <button type="button" name="" class="btn btn-danger float-right" data-dismiss="modal">Tutup</button>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
+</div>
+
+<!-- The modal pengaturan -->
+<div class="modal fade" id="kompenTambah" tabindex="-1" role="dialog" aria-labelledby="modalLabelLarge" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-body">
+        <h5 class="modal-title text-center">Form Konfirmasi Kompensasi</h5>
+        <hr class="pl-4 pr-4 bg-dark">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="tanggal">Tanggal</label>
+              <input type="date" class="form-control" id="tanggal">
+            </div>
+            <div class="form-group">
+              <label for="jeniskompensasi">Jenis Kompensasi </label>
+              <textarea class="form-control" rows="5" id="jeniskompensasi"></textarea>
+            </div>
+
+            <div class="form-group">
+              <label for="totaljam">Total Jam</label>
+              <input type="text" class="form-control" id="totaljam">
+            </div>
+            <div class="form-group">
+              <div>
+                <label for="jeniskelamin">Dosen</label>
+                <select id="jeniskelamin" class="form-control">
+                  <option value="lakilaki">Dosen A</option>
+                  <option value="perempuan">Dosen B</option>
+                  <option value="perempuan">Dosen C</option>
+                  <option value="perempuan">Dosen D</option>
+                  <option value="perempuan">Dosen E</option>
+                  <option value="perempuan">Dosen F</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="password">Password:</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Delete</button>
+        <button type="button" class="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
