365b137d93087d4b0fdc4387966347f29154156c
bluohazard <greggygf@gmail.com>
2019-04-24 21:49:14 +0700

    menambah fungsi jika mencari jadwal kuliah semester tertentu

--- process/proses_jadwalKuliah.php
+++ process/proses_jadwalKuliah.php
@@ -14,6 +14,20 @@ function jadwalKuliah($con,$prodi,$kelas)
     return $resultJadwalKuliah;
 }
 
+function jadwalKuliahCariSemester($con,$prodi,$kelas,$semester)
+{
+    $jadwalKuliahCariSemester = "select tm.nama as nm_matkul,td.nama,tj.hari,tm.sks,tm.jam,tr.kode,tr.lantai from tabel_jadwal tj,tabel_matkul tm,tabel_dosen td,tabel_ruang tr
+    where tj.id_matkul = tm.id_matkul
+    and tj.id_dosen = td.id_dosen
+    and tj.id_ruang = tr.id_ruang
+    and tj.id_kelas = $kelas
+    and tj.id_prodi = $prodi
+    and tj.id_semester = $semester;";
+
+    $resultJadwalKuliahCariSemester = mysqli_query($con,$jadwalKuliahCariSemester);
+    return $resultJadwalKuliahCariSemester;
+}
+
 function semester($con)
 {
     $semester = "select * from tabel_semester";
