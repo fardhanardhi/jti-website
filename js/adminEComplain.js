4e0c82af1286d7c85048749f0e725fd20529b23b
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-04-24 22:20:57 +0700

    memperbaiki chat e-complain apabila diisi kosong

--- js/adminEComplain.js
+++ js/adminEComplain.js
@@ -45,25 +45,30 @@ $(document).ready(function() {
   $(document).on("click", ".recent-chat-item", function() {
     idUserTujuan = $(this).data("id");
     reload();
+    $("#inputChat").prop("disabled", false);
   });
 
   $(document).on("click", ".btn-send", function() {
     var isiChat = $("#inputChat").val();
     var idUser = $("#idUser").val();
-    $.ajax({
-      url: "../process/proses_eComplain.php",
-      type: "POST",
-      data: {
-        sendChat: 1,
-        isiChat: isiChat,
-        idUser: idUser,
-        idUserTujuan: idUserTujuan
-      },
-      success: function(response) {
-        $("#inputChat").val("");
-        reload();
-      }
-    });
+    if ($.trim(isiChat) != "") {
+      $.ajax({
+        url: "../process/proses_eComplain.php",
+        type: "POST",
+        data: {
+          sendChat: 1,
+          isiChat: isiChat,
+          idUser: idUser,
+          idUserTujuan: idUserTujuan
+        },
+        success: function(response) {
+          $("#inputChat").val("");
+          reload();
+        }
+      });
+    } else {
+      $("#inputChat").val("");
+    }
   });
 
   function reload() {
