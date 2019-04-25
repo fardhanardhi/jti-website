6ccd3189e9e8451cea65f11d2fc2c68cdd1f51a9
lenisaputri <lenisaputri98@gmail.com>
2019-04-25 17:47:41 +0700

    Membenarkan pencarian, menganti select option, menganti row nm_mahasiswa

--- module/admin/khs/khsLihat.php
+++ module/admin/khs/khsLihat.php
@@ -52,8 +52,12 @@
                 <div class="media text-muted pt-8">
                     <div class="media-body pb-8 mb-0">
                     <?php
-                        $resultTampilKhsLihat=khsLihat($con);
-                        if(mysqli_num_rows($resultTampilKhsLihat) > 0){
+                        if(isset($_POST["cariKhs"])){
+                        $result=khs($con, $_POST["kelas"], $_POST["semester"]);
+                        }else{
+                        $result = khsLihat($con);
+                        }
+                        if(mysqli_num_rows($result) > 0){
                         ?>
                         <table class="table table-striped table-bordered text-center">
                             <thead>
@@ -68,7 +72,7 @@
                             <tbody>
                             <?php 
                                 $no=1;
-                                while($row = mysqli_fetch_assoc($resultTampilKhsLihat)){
+                                while($row = mysqli_fetch_assoc($result)){
                                     ?>
                                 <tr>
                                     <td><?php echo $no;?></td>
