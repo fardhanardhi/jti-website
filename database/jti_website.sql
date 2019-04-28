8ded30b6333512bf06acb05d2404d8aaa7824fbb
songyurim <chandika.salsabila@gmail.com>
2019-04-28 19:19:53 +0700

    update database ganti nama tabel_krs_admin jadi tabel_krs

--- database/jti_website.sql
+++ database/jti_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2019 at 09:33 AM
+-- Generation Time: Apr 28, 2019 at 02:18 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
@@ -419,50 +419,28 @@ INSERT INTO `tabel_kompen` (`id_kompen`, `id_mahasiswa`, `id_dosen`, `id_semeste
 
 CREATE TABLE `tabel_krs` (
   `id_krs` int(30) NOT NULL,
-  `id_mahasiswa` int(30) NOT NULL,
-  `id_matkul` int(100) DEFAULT NULL,
-  `waktu_edit` datetime NOT NULL
+  `id_mahasiswa` int(30) DEFAULT NULL,
+  `status_daftar_ulang` varchar(50) DEFAULT NULL,
+  `gambar_krs` varchar(100) DEFAULT NULL,
+  `id_semester` int(30) DEFAULT NULL,
+  `waktu_edit` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tabel_krs`
 --
 
-INSERT INTO `tabel_krs` (`id_krs`, `id_mahasiswa`, `id_matkul`, `waktu_edit`) VALUES
-(1, 33, 1, '2019-04-09 00:00:00'),
-(2, 33, 2, '2019-04-09 00:00:00'),
-(3, 33, 4, '2019-04-09 00:00:00'),
-(4, 33, 6, '2019-04-09 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tabel_krs_admin`
---
-
-CREATE TABLE `tabel_krs_admin` (
-  `id_krs` int(30) NOT NULL,
-  `id_mahasiswa` int(30) DEFAULT NULL,
-  `status_daftar_ulang` varchar(50) DEFAULT NULL,
-  `gambar_krs` varchar(100) DEFAULT NULL,
-  `id_semester` int(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tabel_krs_admin`
---
-
-INSERT INTO `tabel_krs_admin` (`id_krs`, `id_mahasiswa`, `status_daftar_ulang`, `gambar_krs`, `id_semester`) VALUES
-(1, 33, 'Sudah', '20190428093312.png', 7),
-(2, 34, 'Belum', NULL, 7),
-(3, 35, 'Sudah', '20190428093318.png', 7),
-(4, 36, 'Belum', NULL, 7),
-(5, 37, 'Sudah', NULL, 7),
-(6, 38, 'Belum', NULL, 7),
-(7, 39, 'Sudah', NULL, 7),
-(8, 40, 'Belum', NULL, 7),
-(9, 41, 'Sudah', NULL, 7),
-(10, 42, 'Belum', NULL, 7);
+INSERT INTO `tabel_krs` (`id_krs`, `id_mahasiswa`, `status_daftar_ulang`, `gambar_krs`, `id_semester`, `waktu_edit`) VALUES
+(1, 33, 'Sudah', '20190428093312.png', 7, NULL),
+(2, 34, 'Belum', NULL, 7, NULL),
+(3, 35, 'Sudah', '20190428093318.png', 7, NULL),
+(4, 36, 'Belum', NULL, 7, NULL),
+(5, 37, 'Sudah', NULL, 7, NULL),
+(6, 38, 'Belum', NULL, 7, NULL),
+(7, 39, 'Sudah', NULL, 7, NULL),
+(8, 40, 'Belum', NULL, 7, NULL),
+(9, 41, 'Sudah', NULL, 7, NULL),
+(10, 42, 'Belum', NULL, 7, NULL);
 
 -- --------------------------------------------------------
 
@@ -937,14 +915,6 @@ ALTER TABLE `tabel_kompen`
 ALTER TABLE `tabel_krs`
   ADD PRIMARY KEY (`id_krs`),
   ADD KEY `id_mahasiswa` (`id_mahasiswa`),
-  ADD KEY `id_matkul` (`id_matkul`);
-
---
--- Indexes for table `tabel_krs_admin`
---
-ALTER TABLE `tabel_krs_admin`
-  ADD PRIMARY KEY (`id_krs`),
-  ADD KEY `id_mahasiswa` (`id_mahasiswa`),
   ADD KEY `id_semester` (`id_semester`);
 
 --
@@ -1110,12 +1080,6 @@ ALTER TABLE `tabel_kompen`
   MODIFY `id_kompen` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `tabel_krs`
---
-ALTER TABLE `tabel_krs`
-  MODIFY `id_krs` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `tabel_kuisioner`
 --
 ALTER TABLE `tabel_kuisioner`
@@ -1279,16 +1243,9 @@ ALTER TABLE `tabel_kompen`
 -- Constraints for table `tabel_krs`
 --
 ALTER TABLE `tabel_krs`
-  ADD CONSTRAINT `tabel_krs_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `tabel_mahasiswa` (`id_mahasiswa`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tabel_krs_ibfk_2` FOREIGN KEY (`id_matkul`) REFERENCES `tabel_matkul` (`id_matkul`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `tabel_krs_admin`
---
-ALTER TABLE `tabel_krs_admin`
-  ADD CONSTRAINT `tabel_krs_admin_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `tabel_mahasiswa` (`id_mahasiswa`),
-  ADD CONSTRAINT `tabel_krs_admin_ibfk_2` FOREIGN KEY (`id_mahasiswa`) REFERENCES `tabel_mahasiswa` (`id_mahasiswa`),
-  ADD CONSTRAINT `tabel_krs_admin_ibfk_3` FOREIGN KEY (`id_semester`) REFERENCES `tabel_semester` (`id_semester`);
+  ADD CONSTRAINT `tabel_krs_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `tabel_mahasiswa` (`id_mahasiswa`),
+  ADD CONSTRAINT `tabel_krs_ibfk_2` FOREIGN KEY (`id_mahasiswa`) REFERENCES `tabel_mahasiswa` (`id_mahasiswa`),
+  ADD CONSTRAINT `tabel_krs_ibfk_3` FOREIGN KEY (`id_semester`) REFERENCES `tabel_semester` (`id_semester`);
 
 --
 -- Constraints for table `tabel_mahasiswa`
