7fd5f069e92eabeedde960f00fff9be4483755b1
bluohazard <greggygf@gmail.com>
2019-04-29 11:25:31 +0700

    mengubah query untuk menampilkan jadwalKuliah

--- process/proses_adminJadwalKuliah.php
+++ process/proses_adminJadwalKuliah.php
@@ -5,7 +5,6 @@ function jadwalKuliah($con)
 {
     $jadwalKuliah = "select tj.id_kelas,tp.kode,tk.tingkat,tk.kode_kelas,tp.nama,ts.semester,count(tj.id_matkul) as jumlah_matkul,sum(tm.sks) as jumlah_sks from tabel_jadwal tj,tabel_kelas tk,tabel_prodi tp,tabel_semester ts,tabel_matkul tm
     where tj.id_kelas = tk.id_kelas
-    and tj.id_prodi = tp.id_prodi
     and tj.id_semester = ts.id_semester
     and tj.id_matkul = tm.id_matkul
     group by tj.id_kelas";
