df72dcec56c63b091790e2e994deaa557bc1e0a6
bluohazard <greggygf@gmail.com>
2019-04-29 12:31:23 +0700

    mengubah query untuk menampilkan jadwal kuliah & fungsi utk insert data

--- process/proses_adminJadwalKuliah.php
+++ process/proses_adminJadwalKuliah.php
@@ -3,11 +3,14 @@ include "../config/connection.php";
 
 function jadwalKuliah($con)
 {
-    $jadwalKuliah = "select tj.id_kelas,tp.kode,tk.tingkat,tk.kode_kelas,tp.nama,ts.semester,count(tj.id_matkul) as jumlah_matkul,sum(tm.sks) as jumlah_sks from tabel_jadwal tj,tabel_kelas tk,tabel_prodi tp,tabel_semester ts,tabel_matkul tm
+    $jadwalKuliah = "select tj.id_kelas,tp.kode,tk.tingkat,tk.kode_kelas,tp.nama,ts.semester,count(tj.id_matkul) as jumlah_matkul,sum(tm.sks) as jumlah_sks from tabel_jadwal tj,tabel_kelas tk,tabel_ruang tr,tabel_semester ts,tabel_dosen td,tabel_matkul tm,tabel_prodi tp
     where tj.id_kelas = tk.id_kelas
+    and tj.id_ruang = tr.id_ruang
     and tj.id_semester = ts.id_semester
+    and tj.id_dosen = td.id_dosen
     and tj.id_matkul = tm.id_matkul
-    group by tj.id_kelas";
+    and tk.id_prodi = tp.id_prodi
+    group by tj.id_semester,tj.id_kelas";
 
     $resultJadwalKuliah = mysqli_query($con,$jadwalKuliah);
     return $resultJadwalKuliah;
@@ -47,12 +50,21 @@ function kelas($con)
     return $resultKelas;
 }
 
+function semester($con)
+{
+    $semester = "select * from tabel_semester";
+
+    $resultSemester = mysqli_query($con,$semester);
+    return $resultSemester;
+}
+
 if (isset($_POST["insert"]) || isset($_POST["delete"]))
 {
     $id_ruang = $_POST['id_ruang'];
     $id_kelas = $_POST['id_kelas'];
     $id_dosen = $_POST['id_dosen'];
     $id_matkul = $_POST['id_matkul'];
+    $id_semester = $_POST['id_semester'];
     $hari = $_POST['hari'];
     $jam_mulai = $_POST['jam_mulai'];
     $jam_selesai = $_POST['jam_selesai'];
@@ -62,7 +74,7 @@ if (isset($_POST["insert"]) || isset($_POST["delete"]))
 
         $queryInsert =   "INSERT INTO tabel_jadwal (id_ruang, id_kelas, id_semester, id_dosen, id_matkul, hari, jam_mulai, jam_selesai, waktu_edit)
 
-        values ('$id_ruang','$id_kelas','7','$id_dosen','$id_matkul','$hari','$jam_mulai','$jam_selesai',now())";
+        values ('$id_ruang','$id_kelas','$id_semester','$id_dosen','$id_matkul','$hari','$jam_mulai','$jam_selesai',now())";
 
         mysqli_query($con, $queryInsert);
 
