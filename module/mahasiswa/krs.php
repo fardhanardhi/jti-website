bd5e35066cd5cf77f679097aaae03dfc0d2ac8a4
bluohazard <greggygf@gmail.com>
2019-05-03 23:21:23 +0700

    menampilkan krs sesuai database ( belum fix )

--- module/mahasiswa/krs.php
+++ module/mahasiswa/krs.php
@@ -17,8 +17,13 @@ $kodeProdiUser = $rowUser["kode"];
 $tingkatUser = $rowUser["tingkat"];
 $kodeKelasUser = $rowUser["kode_kelas"];
 
+$idMahasiswaUser = $rowUser["id_mahasiswa"];
+
 // $prodiUser = $rowUser["id_prodi"];
 // $kelasUser = $rowUser["id_kelas"];
+
+$row = 0;
+$statusVerifikasi = '';
 
 ?>
 
@@ -50,20 +55,29 @@ $kodeKelasUser = $rowUser["kode_kelas"];
 
         <div class="col-md-9 p-0">
             <div class="m-2 p-3 bg-white rounded shadow-sm">
-                <h6 class="border-bottom border-gray pb-2 mb-2">SEMESTER 4 (2019/2020) | <?php echo $namaProdiUser; ?> - <?php echo $kodeProdiUser; ?>-<?php echo $tingkatUser; echo $kodeKelasUser; ?></h6>
+                <h6 class="border-bottom border-gray pb-2 mb-2">SEMESTER 4 (2019/2020) | <?php echo $namaProdiUser; ?> -
+                    <?php echo $kodeProdiUser; ?>-<?php echo $tingkatUser; echo $kodeKelasUser; ?></h6>
                 <center>
+                    <?php 
+                    if($row != 0)
+                    {
+                    ?>
                     <div class="warna-card col-md-12 mt-3 p-2">
                         <p class="card-title">Belum terverifikasi oleh DPA</p>
                     </div>
+                    <?php
+                    }
+                    ?>
                 </center>
                 <div class="media text-muted pt-3">
                     <p class="media-body pb-3 mb-0 small lh-125">
                         <strong class="d-block text-dark">Periode Semester</strong>
                     </p>
                 </div>
-                <select class="semester custom-select" style="width:250px" name="semester">
-                    <option selected disabled>-</option>
-                    <?php 
+                <form action="?module=krs" class="p-0 m-0" method="post">
+                    <select class="semester custom-select" style="width:250px" name="semester">
+                        <option selected disabled>-</option>
+                        <?php 
                         $resultSemester=semester($con); 
                         if(mysqli_num_rows($resultSemester))
                         {
@@ -78,29 +92,60 @@ $kodeKelasUser = $rowUser["kode_kelas"];
                                 $selected = "";
                             }
                             ?>
-                            <option <?php echo $selected; ?> value="<?php echo $rowSemester["id_semester"];?>">
+                        <option <?php echo $selected; ?> value="<?php echo $rowSemester["id_semester"];?>">
                             Semester <?php echo $rowSemester["semester"];?>
-                            </option>
-                            <?php
+                        </option>
+                        <?php
                             }
                         }
                     ?>
-                </select>
-                <button type="submit" name="cariKrs" class="tmbl-filter btn btn-success ml-2">Filter</button>
-                <button type="button" class="btn btn-success float-right">Kirim ke DPA &nbsp&nbsp<i
+                    </select>
+                    <button type="submit" name="cariKrs" class="tmbl-filter btn btn-success ml-2">Filter</button>
+                    <?php
+                    if($statusVerifikasi == "Belum")
+                    {
+                    ?>
+                    <button type="button" class="btn btn-success float-right">Kirim ke DPA &nbsp&nbsp<i
                         class="fas fa-arrow-circle-up"></i></button>
+                    <?php
+                    }
+                    ?>
+                </form>
+
+                
                 <br><br>
+                <?php
+                if(isset($_POST["cariKrs"]))
+                {
+                    $resultKrs = krsCariSemester($con,$idMahasiswaUser,$_POST["semester"]);
+                    $row = mysqli_fetch_assoc($resultKrs);
+                    $statusVerifikasi = $row["status_verifikasi"];
+                }
+                ?>
 
-                <img src="../attachment/img/krs.png" width="100%" alt="">
-
-                <center>
-                    <div class="warna-card col-md-12 border border-danger mt-3">
-                        <div class="teks card-body" style="position: center">
-                            <!-- <p class="card-title">| <img src="../img/navigation/icon.svg"></a> Informasi|</p> -->
-                            <p class="card-title">| <i class="fas fa-info"></i> Informasi |</p>
-                            <p class="card-text" style="color:#950101">*Tidak dapat menampilkan data*</p>
-                            <p class="card-text">- Mahasiswa wajib membayar UKT sebelum masa KRS</p>
-                        </div>
-                </center>
+                <?php  
+                if($row["status_daftar_ulang"] == "Sudah")
+                {
+                ?>
+                    <img src="../attachment/img/krs.png" width="100%" alt="">
+                <?php
+                }
+                else if($row["status_daftar_ulang"] == "Belum")
+                {
+                ?>
+                    <center>
+                        <div class="warna-card col-md-12 border border-danger mt-3">
+                            <div class="teks card-body" style="position: center">
+                                <!-- <p class="card-title">| <img src="../img/navigation/icon.svg"></a> Informasi|</p> -->
+                                <p class="card-title">| <i class="fas fa-info"></i> Informasi |</p>
+                                <p class="card-text" style="color:#950101">*Tidak dapat menampilkan data*</p>
+                                <p class="card-text">- Mahasiswa wajib membayar UKT sebelum masa KRS</p>
+                            </div>
+                    </center>
+                <?php
+                }
+                else
+                {}
+                ?>
             </div>
         </div>
