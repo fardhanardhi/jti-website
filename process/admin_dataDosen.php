3890d48dbeafba37ee5e3e2820fd2fc8579340fd
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2019-05-13 21:39:09 +0700

    fix chart pada dashboard admin

---
+++ process/admin_dataDosen.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json');
+include '../config/connection.php';
+
+$query = "SELECT YEAR(waktu_tambah) AS tahun, COUNT(*) AS jumlah FROM tabel_dosen GROUP BY YEAR(waktu_tambah) ORDER BY tahun ASC LIMIT 6";
+
+$result = mysqli_query($con, $query);
+
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+mysqli_close($conn);
+
+echo json_encode($data);
+?>
