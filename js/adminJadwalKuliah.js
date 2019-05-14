bf0f31f88988e40df3d0dbe4a0920bd362afa15d
bluohazard <greggygf@gmail.com>
2019-05-14 23:59:38 +0700

    menambah fungsi utk edit jadkul

--- js/adminJadwalKuliah.js
+++ js/adminJadwalKuliah.js
@@ -90,3 +90,23 @@ $(".tampil-detail").click(function () {
     }
   });
 });
+
+$(".edit-jadwal-kuliah").click(function () {
+  var id_kelas = $(this).attr("data-kelas");
+  var id_semester = $(this).attr("data-semester");
+
+  $.ajax({
+    url: "../process/proses_adminJadwalKuliah.php",
+    method: "post",
+    data: {
+      editJadwal_kelas: id_kelas,
+      editJadwal_semester: id_semester
+    },
+    success: function (data) {
+      $("#id_kelasEdit").val(id_kelas);
+      $("#id_semesterEdit").val(id_semester);
+      $("#edit-jadwalKuliah").html(data);
+      $("#modalEdit").modal("show");
+    }
+  });
+});
